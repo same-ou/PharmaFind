1f62b3f042970c5ca539ea91a0e2b9dab9d5dcab
OUSSAMA EL HADRAMI <elhadrami.oussama@ensam-casa.ma>
2024-05-21 15:40:13 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# 💊  PharmaFind: Your Go-To App for Locating Pharmacies and Medicines in Morocco
+
+PharmaFind is a web application designed to help users easily locate nearby pharmacies and check the availability of specific medications in Morocco.
+
---
+++ src/main/java/com/ensam/pharmafind/PharmaFindApplication.java
@@ -0,0 +1,13 @@
+package com.ensam.pharmafind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaFindApplication.class, args);
+	}
+
+}
