ea79c8cf8e7cca1a11432514c1afc362d83a86bc
OUSSAMA EL HADRAMI <elhadrami.oussama@ensam-casa.ma>
2024-06-21 18:43:43 +0100

    File Storage Service Setting

--- backend/src/main/java/com/ensam/pharmafind/controllers/ProductController.java
+++ backend/src/main/java/com/ensam/pharmafind/controllers/ProductController.java
@@ -3,16 +3,20 @@ package com.ensam.pharmafind.controllers;
 import com.ensam.pharmafind.dto.requests.ProductRequest;
 import com.ensam.pharmafind.dto.responses.PageResponse;
 import com.ensam.pharmafind.dto.responses.ProductResponse;
+import com.ensam.pharmafind.service.MinioService;
 import com.ensam.pharmafind.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
+    private final MinioService minioService;
 
     @GetMapping
     public ResponseEntity<PageResponse<ProductResponse>> getProducts(
@@ -43,10 +47,21 @@ public class ProductController {
         return ResponseEntity.ok(productService.saveProduct(productRequest));
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(
             @PathVariable Integer id) {
         productService.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> whuploadProductImage(@RequestParam("file") MultipartFile file) {
+        try {
+            String imageUrl = minioService.uploadFile(file);
+            return ResponseEntity.ok(imageUrl);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload image");
+        }
+    }
+
 }
