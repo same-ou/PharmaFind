6a6a219e8ef46c913b3fcf3e27142bfdfb29b838
Achraf <achrafchahboune8@gmail.com>
2024-06-26 00:21:01 +0100

    last update

--- backend/src/main/java/com/ensam/pharmafind/controllers/ProductController.java
+++ backend/src/main/java/com/ensam/pharmafind/controllers/ProductController.java
@@ -30,12 +30,6 @@ public class ProductController {
         return ResponseEntity.ok(productService.getProducts(page, size));
     }
 
-//    @GetMapping("{id}")
-//    public ResponseEntity<ProductResponse> getProduct(
-//            @RequestParam(name = "id") Integer id
-//    ) {
-//        return ResponseEntity.ok(productService.getProduct(id));
-//    }
 
     @GetMapping("{id}")
     public ResponseEntity<ProductResponse> getProduct(@PathVariable Integer id) {
